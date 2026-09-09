b55ba06b77c0898129da31dd10623e66a610e0a0
tthayer93 <tthayer93@gmail.com>
2026-09-09 17:24:26 +0000

    jf-patch 0084: backport fixes for detecting ac4 streams in mpegts
    Origin: jellyfin-ffmpeg v8.1.2-4 patch 0084
    Upstream: 1e9dd2b7e964f785e4f815bd45ff33ab70288c54 (fuzzy: context skew)

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -2275,6 +2275,7 @@ int ff_parse_mpeg2_descriptor(AVFormatContext *fc, AVStream *st, int stream_type
                 sti->need_parsing = AVSTREAM_PARSE_FULL;
                 sti->need_context_update = 1;
             }
+            break;
         }
         if (ext_desc_tag == SUPPLEMENTARY_AUDIO_DESCRIPTOR) {
             int flags;
@@ -2312,22 +2313,14 @@ int ff_parse_mpeg2_descriptor(AVFormatContext *fc, AVStream *st, int stream_type
                 if (language[0])
                     av_dict_set(&st->metadata, "language", language, 0);
             }
+            break;
+        }
+        if (ext_desc_tag == AC4_DESCRIPTOR) {
+            st->codecpar->codec_id = AV_CODEC_ID_AC4;
+            st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
         }
         break;
     case AC3_DESCRIPTOR:
-        {
-            int component_type_flag = get8(pp, desc_end) & (1 << 7);
-            if (component_type_flag) {
-                int component_type = get8(pp, desc_end);
-                int service_type_mask = 0x38;  // 0b00111000
-                int service_type = ((component_type & service_type_mask) >> 3);
-                if (service_type == 0x02 /* 0b010 */) {
-                    st->disposition |= AV_DISPOSITION_DESCRIPTIONS;
-                    av_log(ts ? ts->stream : fc, AV_LOG_DEBUG, "New track disposition for id %u: %u\n", st->id, st->disposition);
-                }
-            }
-        }
-        break;
     case ENHANCED_AC3_DESCRIPTOR:
         {
             int component_type_flag = get8(pp, desc_end) & (1 << 7);
